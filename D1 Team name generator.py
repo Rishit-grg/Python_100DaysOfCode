b1e69a01ba261a2620810ae25209293a124ff95c
Rishit Garg <rishitalig@gmail.com>
2025-12-24 02:44:12 +0530

    Day 1

---
+++ D1 Team name generator.py
@@ -0,0 +1,5 @@
+print("welcome to Hackathon Team Name Generator")
+a = input("What Compsci term sounds the coolest to you? ")
+b = input("What is your favourite animal? ")
+
+print(f"Your team should be called - 'THE {a.strip().upper()} {b.strip().upper()}S'")  # Chained the strip and the upper function
